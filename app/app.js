2ca307e879ee6447c390dd4c7f34da1f4997d197
Pat Migliaccio <patmigliaccio48@gmail.com>
2016-06-13 22:57:09 -0400

    proxy added with http interceptors

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 angular.module('ShipperApp', ['ngRoute', 'ngSanitize', 'ngCsv'])
-    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    .config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
         $routeProvider
             .when('/orders', {
                 templateUrl: 'app/views/ordersView.html',
@@ -15,7 +15,20 @@ angular.module('ShipperApp', ['ngRoute', 'ngSanitize', 'ngCsv'])
 
         //TODO get html5mode working
         $locationProvider.html5Mode(false).hashPrefix('!');
-        
+
+        //uses proxy for http requests
+        $httpProvider.interceptors.push(function ($q) {
+            return {
+                'request': function (config) {
+                    if (!~config.url.indexOf('/views/')){  //ignores ngRoute requests
+                        config.url = 'proxy.php?url=' + config.url + '&mode=native';
+                    }
+
+                    return config || $q.when(config);
+                }
+
+            }
+        });
     }]);
 
 angular.module('Authentication', []);
--- app/services/shipstationService.js
+++ app/services/shipstationService.js
@@ -1,29 +1,27 @@
 (function () {
     'use strict';
+
+    //TODO setup authService along with login as a modal and pass in header then read in proxy
     var authKey = "{{ AuthKey }}";
     
     var app = angular.module('ShipperApp');
 
+    //TODO move headers to $httpProvider
     var config = {
         'Authorization': 'Basic ' + authKey,
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Content-Type': 'application/json'
     };
-
+    
     app.factory('ShipStation', ['$http', function($http){
         return {
             getOrders: function(){
-                return $http({
-                    url: 'data/orders.json',
-                    method: 'GET',
-                    withCredentials: true,
-                    headers: config
-                })
-                .then(function(response){
-                    return response.data.orders;
-                }, function(error) {
-                    console.log(error);
-                });
+                return $http.get('https://ssapi.shipstation.com/orders?orderStatus=awaiting_shipment') // data/orders.json (for static)
+                        .then(function(response){
+                            return response.data.orders;
+                        }, function(error) {
+                            console.log(error);
+                        });
             }
         }
 
