25700e5d7f948ded4bf4f279e2b153647da3034f
Pat Migliaccio <patmigliaccio48@gmail.com>
2016-06-29 22:32:51 -0400

    special cases added to totaling algorithm

--- app/orders/orders.js
+++ app/orders/orders.js
@@ -2,6 +2,15 @@ angular.module('orders', ['resources.orders'])
     .constant('totalsConfig', {
             specialCase: "{{ ItemName }}", //adds items from nested options
             removeParent: true, //removes parent item from list if nested item found
+
+            nameReplacePattern: "gift|subscription", //removes these values from item names
+
+            specialItemName: "{{ ItemName }}", //if item name is this
+            specialItemNewName: "{{ ItemName }}", //replaces it with this
+            specialItemWeight: "5lb|5 lb", //if special item weight matches this
+            specialItemNewWeight: "3.6", //replace it with this
+            specialItemNewWeightUnits: "lbs", //and unit with these
+
             defaultProductWeight: "12", //sets a default weight for new nested products added
             defaultProductUnits: "ounces",//sets a default unit value for new nested products added
             displayWeightAs: "lbs" //converts totals to this unit of measurement
@@ -68,7 +77,7 @@ angular.module('orders', ['resources.orders'])
                     var productWeight = totalsConfig.defaultProductWeight; //sets a default weight for new nested products added
                     var productUnits = totalsConfig.defaultProductUnits; //sets a default unit value for new nested products added
                     var weightFilter = totalsConfig.displayWeightAs; //converts totals to this unit of measurement
-        
+
                     for (var x in orders) {
         
                         var items = [];
@@ -94,6 +103,7 @@ angular.module('orders', ['resources.orders'])
         
                                     items.push({
                                         name: option.value,
+                                        sku: option.value,
                                         weight: {
                                             value: productWeight,
                                             units: productUnits
@@ -106,25 +116,66 @@ angular.module('orders', ['resources.orders'])
                                     skipItem = removeParent && true;
                                 }
                             }
-        
-                            if (!skipItem && items[i].weight.value > 0){
-                                if (!v[items[i].name]) {
-                                    v[items[i].name] = {
-                                        item_name: items[i].name,
+
+                            //if not a nested option and weight is more than 0
+                            if (!skipItem && items[i].weight.value > 0) {
+                                //if name contains regex remove words
+                                var pattern = new RegExp(totalsConfig.nameReplacePattern, 'gi'); //case insensitive
+                                var itemName = items[i].name.replace(pattern, "").trim();
+
+                                var itemSKU = items[i].sku;
+
+                                //if item name matches regex replace entire name
+                                var nameRegex = new RegExp(totalsConfig.specialItemName, 'gi');
+                                if (itemName.match(nameRegex)) {
+                                    itemName = totalsConfig.specialItemNewName;
+
+                                    //if sku contains regex change to new weight
+                                    var skuRegex = new RegExp(totalsConfig.specialItemWeight, 'gi');
+                                    if (itemSKU.match(skuRegex)) {
+                                        items[i].weight.value = totalsConfig.specialItemNewWeight;
+                                        items[i].weight.units = totalsConfig.specialItemNewWeightUnits;
+                                    }
+                                }
+
+                                //if not already in final array add it
+                                if (!v[itemName]) {
+                                    v[itemName] = {
+                                        item_name: itemName,
+                                        twelve_ounce_count: 0,
+                                        five_pound_count: 0,
+                                        twelve_sub_count: 0,
+                                        five_sub_count: 0,
                                         total_weight: 0,
                                         item_weight_units: weightFilter
                                     };
                                 }
-        
-                                //converts ounces to pounds
+
+                                //sum different product types
+                                if (itemSKU.indexOf('Subscription') > -1 || itemSKU.indexOf('Sub') > -1) {
+                                    if (itemSKU.indexOf('5lb') > -1 || itemSKU.indexOf('5 lb') > -1) {
+                                        v[itemName].five_sub_count++;
+                                    } else {
+                                        v[itemName].twelve_sub_count++;
+                                    }
+                                } else {
+                                    if (itemSKU.indexOf('5lb') > -1 || itemSKU.indexOf('5 lb') > -1) {
+                                        v[itemName].five_pound_count++;
+                                    } else {
+                                        v[itemName].twelve_ounce_count++;
+                                    }
+                                }
+
+                                //convert ounces to pounds if filter is set to lbs
                                 if (items[i].weight.units == "ounces" && weightFilter == "lbs") {
                                     items[i].weight.value = Number(items[i].weight.value) / 16;
                                 }
-        
-                                v[items[i].name].total_weight += Number(items[i].weight.value);
+
+                                //adds weight to total
+                                v[itemName].total_weight += Number(items[i].weight.value);
                             }
-        
                         }
+        
                     }
         
                     return $.map(v, function (value) {
