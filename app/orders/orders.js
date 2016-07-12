15a687f63f0a8d936891452db5a5899ed4ffbd48
Pat Migliaccio <patmigliaccio48@gmail.com>
2016-07-11 22:51:14 -0400

    totaling algorithm to use '-' delimited naming convention

--- app/orders/orders.js
+++ app/orders/orders.js
@@ -1,24 +1,23 @@
 angular.module('orders', ['resources.orders'])
     .constant('cfg', {
-            specialCase: "{{ ItemName }}", //adds items from nested options
+            nestedItemSKUPrefix: "",
+            specialCase: "", //adds items from nested options
             removeParent: true, //removes parent item from list if nested item found
 
-            nameReplacePattern: "gift|subscription", //removes these values from item names
-
-            specialItemName: "{{ ItemName }}", //if item name matches this
-            specialItemNewName: "{{ ItemName }}", //replaces it with this
-            specialItemWeight: "5lb|5 lb", //if special item weight matches this
-            specialItemNewWeight: "3.6", //replace it with this
+            specialItemSKUPrefix: "", //if item name matches this
+            specialItemNewSKUPrefix: "", //replaces it with this
+            specialItemNewWeight: "", //replace it with this
             specialItemNewWeightUnits: "lbs", //and unit with these
 
-            ignoreItemsNameWith: "{{ IgnoredItem(s) }}", //does not total items that match these keys
+            ignoredItemsSKUPrefix: "", //does not total items that match this sku
 
             defaultProductWeight: "12", //sets a default weight for new nested products added
             defaultProductUnits: "ounces",//sets a default unit value for new nested products added
             displayWeightAs: "lbs", //converts totals to this unit of measurement
 
-            productSortOrder: [
-            ]
+            productKey: {
+
+            }
         })
 
     .controller('OrdersCtrl', 
@@ -65,7 +64,7 @@ angular.module('orders', ['resources.orders'])
                 var init = function () {
                     usSpinnerService.spin('spinner');
         
-                    orders.get({ orderStatus: 'awaiting_shipment' },
+                    orders.get({ orderStatus: 'awaiting_shipment', pageSize: 500 },
                         function(response){
                             tc.totals = tc.Process(response.orders);
                             usSpinnerService.stop('spinner');
@@ -75,8 +74,9 @@ angular.module('orders', ['resources.orders'])
         
                 //totals item weight values that have the same sku
                 tc.Process = function (orders) {
-                    var v = {};
-        
+                    var totals = {};
+
+                    var itemKey = cfg.productKey; //product sku prefixes with corresponding items
                     var specialCase = cfg.specialCase; //adds items from nested options
                     var removeParent = cfg.removeParent; //removes parent item from list if nested item found
                     var productWeight = cfg.defaultProductWeight; //sets a default weight for new nested products added
@@ -86,69 +86,65 @@ angular.module('orders', ['resources.orders'])
                     for (var x in orders) {
         
                         var items = [];
+                        var itemLength = 0;
                         if (orders.hasOwnProperty(x)) {
                             items = orders[x].items;
+                            itemLength = items.length;
                         }
-        
-                        var itemLength = items.length;
-        
+
                         for (var i = 0; i < itemLength; i++) {
-        
-                            //special case for products nested in options
-                            var skipItem = false;
-                            for(var o in items[i].options){
-                                var option = {};
-        
-                                if (items[i].options.hasOwnProperty(o)) {
-                                    option = items[i].options[o];
-                                }
-        
-                                var regex = new RegExp( specialCase, 'gi' ); //case insensitive
-                                if (option.name.match(regex)){
-        
-                                    items.push({
-                                        name: option.value,
-                                        sku: option.value,
-                                        weight: {
-                                            value: productWeight,
-                                            units: productUnits
-                                        },
-                                        options: []
-                                    });
-        
-                                    itemLength++;
-        
-                                    skipItem = removeParent && true;
-                                }
-                            }
+                            var nestedItem = false;
 
-                            var ignoreItem = new RegExp(cfg.ignoreItemsNameWith, 'gi');
+                            var sku = items[i].sku;
+                            //split sku by categorized delimiter
+                            var product = sku.split('-');
 
-                            //if not a nested option and weight is more than 0 and not an ignored item
-                            if (!skipItem && items[i].weight.value > 0 && !items[i].name.match(ignoreItem)) {
-                                //if name contains regex remove words
-                                var pattern = new RegExp(cfg.nameReplacePattern, 'gi'); //case insensitive
-                                var itemName = items[i].name.replace(pattern, "").trim();
+                            //loop through options attributes of products for nested products
+                            if (product[0] == cfg.nestedItemSKUPrefix){
+                                for(var o in items[i].options){
 
-                                var itemSKU = items[i].sku;
-
-                                //if item name matches regex replace entire name
-                                var nameRegex = new RegExp(cfg.specialItemName, 'gi');
-                                if (itemName.match(nameRegex)) {
-                                    itemName = cfg.specialItemNewName;
-
-                                    //if sku contains regex change to new weight
-                                    var skuRegex = new RegExp(cfg.specialItemWeight, 'gi');
-                                    if (itemSKU.match(skuRegex)) {
-                                        items[i].weight.value = cfg.specialItemNewWeight;
-                                        items[i].weight.units = cfg.specialItemNewWeightUnits;
+                                    var itemOption;
+                                    if (items[i].options.hasOwnProperty(o)) {
+                                       itemOption = items[i].options[o];
                                     }
+
+                                    var regex = new RegExp( specialCase, 'gi' ); //case insensitive
+                                    if (itemOption && itemOption.name.match(regex)){
+
+                                        var itemCode = Object.keys(itemKey).filter(function(key) {return itemKey[key] === itemOption.value})[0];
+
+                                        items.push({
+                                            name: itemOption.value,
+                                            sku: itemCode,
+                                            weight: {
+                                                value: productWeight,
+                                                units: productUnits
+                                            },
+                                            options: []
+                                        });
+
+                                        itemLength++;
+
+                                        nestedItem = removeParent && true;
+                                    }
+                                }
+                                //skip if ignored item sku or nested item
+                            } else if (product[0] != cfg.ignoredItemsSKUPrefix && !nestedItem){
+
+                                if (product[0] == cfg.specialItemSKUPrefix) {
+                                    product[0] = cfg.specialItemNewSKUPrefix;
+
+                                    items[i].weight.value = cfg.specialItemNewWeight;
+                                    items[i].weight.units = cfg.specialItemNewWeightUnits;
                                 }
 
                                 //if not already in final array add it
-                                if (!v[itemName]) {
-                                    v[itemName] = {
+                                if (!totals[product[0]]) {
+                                    var itemName = itemKey[product[0]] || product[0];
+
+                                    totals[product[0]] = {
                                         item_name: itemName,
+                                        item_sku: product[0],
                                         twelve_ounce_count: 0,
                                         five_pound_count: 0,
                                         twelve_sub_count: 0,
@@ -159,7 +155,7 @@ angular.module('orders', ['resources.orders'])
                                 }
 
                                 //sum different product types
-                                if (itemSKU.indexOf('Subscription') > -1 || itemSKU.indexOf('Sub') > -1) {
+/*                                if (itemSKU.indexOf('Subscription') > -1 || itemSKU.indexOf('Sub') > -1) {
                                     if (itemSKU.indexOf('5lb') > -1 || itemSKU.indexOf('5 lb') > -1) {
                                         v[itemName].five_sub_count++;
                                     } else {
@@ -171,28 +167,35 @@ angular.module('orders', ['resources.orders'])
                                     } else {
                                         v[itemName].twelve_ounce_count++;
                                     }
+                                }*/
+
+                                //set weight from attribute if it exists
+                                for(var opt in items[i].options){
+                                    if (items[i].options[opt].name.toLowerCase() == "weight") {
+                                        var w = items[i].options[opt].value.split(' ');
+                                        items[i].weight.value = w[0];
+                                        items[i].weight.units = w[1];
+                                    }
                                 }
 
                                 //convert ounces to pounds if filter is set to lbs
-                                if (items[i].weight.units == "ounces" && weightFilter == "lbs") {
+                                if ((items[i].weight.units == "ounces" || items[i].weight.units == "oz") && weightFilter == "lbs") {
                                     items[i].weight.value = Number(items[i].weight.value) / 16;
                                 }
 
                                 //adds weight to total
-                                v[itemName].total_weight += Number(items[i].weight.value);
+                                totals[product[0]].total_weight += Number(items[i].weight.value);
                             }
                         }
         
                     }
 
-                    var sortKey = cfg.productSortOrder;
-
                     //sort object and put in array
-                    var totals = [];
-                    for (var k = 0; i < sortKey.length; i++){
-                        for(var product in v){
-                            if (v[product].item_name.toLowerCase().indexOf(sortKey[k]) > -1){
-                                totals.push(v[product]);
+                    var outTotals = [];
+                    for (var item in itemKey){
+                        for(var obj in totals){
+                            if (obj.toUpperCase().indexOf(item) > -1){
+                                outTotals.push(totals[obj]);
                             }
                         }
                     }
@@ -200,7 +203,7 @@ angular.module('orders', ['resources.orders'])
                     //TODO catch values not in sort key
                     //TODO move sorting to a filter
 
-                    return totals;
+                    return outTotals;
                 };
         
                 init();
